9bddc478d522b9c94a800608a5e8be8390d01113
Mary <mcrowe0331@gmail.com>
2022-09-29 19:03:02 -0500

    updates

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -7,12 +7,19 @@ function Default(html) {
             <title>Default</title>
         </head>
         <body>
-            <h1>HTML Rendered!</h1>
+          <div className="wrapper">
+            <header>
+              <h1><a href="/breads">BreadCRUD</a></h1>
+            </header>
             <div className="container">
                 {html.children}
             </div>
+            </div>
         </body>
     </html>
+
+
+
   )
 }
 
