923179c6aefcd7ad69c8bcbf38592951db95f419
Mary <mcrowe0331@gmail.com>
2022-09-29 19:49:22 -0500

    sending index and show data

--- server.js
+++ server.js
@@ -6,12 +6,11 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
-
-
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 
 // ROUTES
@@ -19,12 +18,19 @@ app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!')
 })
 
+
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
+
 
 // LISTEN
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
+
 console.log(PORT)
